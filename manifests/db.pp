b4e98045d1da6c299c757ee4129a334d7404339d
JoseCarlosNF <joseferreira@ufpa.br>
2020-02-04 16:09:46 -0300

    Fix: Ip host db / db allow access.

--- manifests/db.pp
+++ manifests/db.pp
@@ -15,7 +15,7 @@ file { "mysql-allow-acess":
 	owner   => mysql,
 	group   => mysql,
 	mode    => '0644',
-	content => template("/vagrant/manifests/allow_external.cnf"),
+	content => "[mysqld]\n bind-address = 0.0.0.0\n",
 	require => Package["mysql-server"],
   notify  => Service["mysql"]
 }
@@ -29,22 +29,22 @@ service { "mysql":
 }
 
 exec { "loja-schema":
-  unless  => "mysql -u root loja_schema",
-  command => "mysqladmin -u root create loja_schema",
+  unless  => "mysql -uroot loja_schema",
+  command => "mysqladmin -uroot create loja_schema",
   path    => "/usr/bin/",
   require => Service["mysql"]
 }
 
 exec { "remove-anonymous-user":
-  command => "mysql -u root -e \"DELETE FROM mysql.user WHERE user=’’; FLUSH PRIVILEGES\"",
-  onlyif  => "mysql -u’ ’",
+  command => "mysql -uroot -e \"DELETE FROM mysql.user WHERE user=' '; FLUSH PRIVILEGES\"",
+  onlyif  => "mysql -u' '",
   path    => "/usr/bin",
   require => Service["mysql"]
 }
 
 exec { "loja-user":
-  unless  => "mysql -u loja -p lojasecret loja_schema",
-  command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON loja_schema.* TO 'loja'@'localhost' IDENTIFIED BY 'lojasecret';\"",
+  unless  => "mysql -uloja -plojasecret loja_schema",
+  command => "mysql -uroot -e \"GRANT ALL PRIVILEGES ON loja_schema.* TO 'loja'@'localhost' IDENTIFIED BY 'lojasecret';\"",
   path    => "/usr/bin/",
   require => Exec["loja-schema"]
 }
